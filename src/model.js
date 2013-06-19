b1132b1c3733b6545cb3a9ef977c4031d48df28b
Jeff Dyer <jeffdyer@acm.org>
2013-06-19 07:56:55 -0700

    Whitespace cleanup

--- src/model.js
+++ src/model.js
@@ -23,14 +23,14 @@
 
   The basic usage, assuming an "addition" plugin:
 
-      var node = {op: "+", args: [1, 2]};
-      var model = Model(node).makeDefaultContext();
-	  var latex = model.math("parse", "1+2").math("format", {"color": "blue"});
+    var node = {op: "+", args: [1, 2]};
+    var model = Model(node).makeDefaultContext();
+    var latex = model.math("parse", "1+2").math("format", {"color": "blue"});
 
   Model ASTs have the structure:
 
-      { op: "+", 
-        args: [1, 2], 
+      { op: "+",
+        args: [1, 2],
         style: {"num": blue},
         data: {"key1": "value1", "key2": "value2"},
       }
@@ -38,96 +38,96 @@
 
 (function (target) {
 
-	jQuery.extend (target, {
-		Model : Backbone.Model.extend({
-			append: append,
-			css: css,
-			find: find,
-			initialize: initialize,
-		}),
-	});
+  jQuery.extend (target, {
+    Model : Backbone.Model.extend({
+      append: append,
+      css: css,
+      find: find,
+      initialize: initialize,
+    }),
+  });
 
-	function append(data) {
-		var n = this.node;
-		assert(jQuery.type(n) === "object", "invalid this to Model.append");
-		if (jQuery.type(data)==="object" && data.node!==void 0) {
-			switch (data.node.op) {
-			case "list":
-				n.args.concat(data.node.args);
-				break;
-			default:
-				assert(false, "invalid input to Model.append");
-				break;
-			}
-		}
-		else {
-			n.args.concat(data);
-		}
-	}
+  function append(data) {
+    var n = this.node;
+    assert(jQuery.type(n) === "object", "invalid this to Model.append");
+    if (jQuery.type(data)==="object" && data.node!==void 0) {
+      switch (data.node.op) {
+      case "list":
+        n.args.concat(data.node.args);
+        break;
+      default:
+        assert(false, "invalid input to Model.append");
+        break;
+      }
+    }
+    else {
+      n.args.concat(data);
+    }
+  }
 
-	function css(prop, val) {
-		var n = this.node;
-		if (!n.style) {
-			n.style = {}
-		}
+  function css(prop, val) {
+    var n = this.node;
+    if (!n.style) {
+      n.style = {}
+    }
 
-		if (!val) {
-			if (jQuery.type(prop) === "object") {
-				jQuery.extend(n.style, prop);
-				return this;
-			}
-			else {
-				return n.style[prop];
-			}
-		}
-		else {
-			assert(typeof prop === "string", "Model.css: invalid argument");
-			n.style[prop] = val;
-			return this;
-		}
-	}
+    if (!val) {
+      if (jQuery.type(prop) === "object") {
+        jQuery.extend(n.style, prop);
+        return this;
+      }
+      else {
+        return n.style[prop];
+      }
+    }
+    else {
+      assert(typeof prop === "string", "Model.css: invalid argument");
+      n.style[prop] = val;
+      return this;
+    }
+  }
 
-	function find(selector) {
-		var n = this.node;
-		var a = Model({op: "list", args: []});
-		if (n===void 0) {
-			return a;
-		}
-		if (select(selector, n)) {
-			a.append(n);
-		}
-		jQuery.each(n.args, function (index, n) {
-			if (select(selector, n)) {
-				a.append(n);
-			}
-			a.append(find.call(n, selector));
-		});
-		return a;
-	}
+  function find(selector) {
+    var n = this.node;
+    var a = Model({op: "list", args: []});
+    if (n===void 0) {
+      return a;
+    }
+    if (select(selector, n)) {
+      a.append(n);
+    }
+    jQuery.each(n.args, function (index, n) {
+      if (select(selector, n)) {
+        a.append(n);
+      }
+      a.append(find.call(n, selector));
+    });
+    return a;
+  }
 
-	function initialize() {
-		var node = arguments[0];
-		if (node===void 0) {
-			node = {};
-		}
+  function initialize() {
+    var node = arguments[0];
+    if (node===void 0) {
+      node = {};
+    }
 
-		// initialize the state of 'this' object
-		switch (jQuery.type(node)) {
-		case "string":
-			if (!Model.parse) {
-				jQuery.error("Model.parse not implemented");
-			}
-			node = Model.parse(node);
-			break;
-		case "object":
-			// node is already an AST
-			break;
-		}
+    // initialize the state of 'this' object
+    switch (jQuery.type(node)) {
+    case "string":
+      if (!Model.parse) {
+        jQuery.error("Model.parse not implemented");
+      }
+      node = Model.parse(node);
+      break;
+    case "object":
+      // node is already an AST
+      break;
+    }
 
-		jQuery.extend( this, {
-			node: node,
-		});
+    jQuery.extend( this, {
+      node: node,
+    });
 
-	}
+  }
 
 })(this);
